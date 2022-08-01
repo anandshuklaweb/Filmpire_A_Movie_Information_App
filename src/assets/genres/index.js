aef6876f65ba79f33fe94c4d305f3bb3bcf2b10d
anandshuklaweb <anandshukla.web@gmail.com>
2022-08-01 16:43:46 +0530

    added sidebar, movies by categories and search functionality

---
+++ src/assets/genres/index.js
@@ -0,0 +1,47 @@
+import action from "./action.png";
+import adventure from "./adventure.png";
+import animation from "./animation.png";
+import comedy from "./comedy.png";
+import crime from "./crime.png";
+import documentary from "./documentary.png";
+import drama from "./drama.png";
+import family from "./family.png";
+import fantasy from "./fantasy.png";
+import history from "./history.png";
+import horror from "./horror.png";
+import music from "./music.png";
+import mystery from "./mystery.png";
+import popular from "./popular.png";
+import romance from "./romance.png";
+import scienceFiction from "./science fiction.png";
+import thriller from "./thriller.png";
+import topRated from "./toprated.png";
+import tvMovie from "./tvmovie.png";
+import upcoming from "./upcoming.png";
+import war from "./war.png";
+import western from "./western.png";
+
+export default {
+  action,
+  adventure,
+  animation,
+  comedy,
+  crime,
+  documentary,
+  drama,
+  family,
+  fantasy,
+  history,
+  horror,
+  music,
+  mystery,
+  popular,
+  romance,
+  "science fiction": scienceFiction,
+  thriller,
+  "tv movie": tvMovie,
+  upcoming,
+  "top rated": topRated,
+  war,
+  western,
+};
