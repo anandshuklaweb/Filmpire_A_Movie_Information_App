5c57f8e615d15f290c5bf0da55827a56daec8380
anandshuklaweb <anandshukla.web@gmail.com>
2022-08-03 11:41:46 +0530

    Implemented Alan Ai

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useRef } from "react";
 import { CssBaseline } from "@mui/material";
 import { Route, Routes } from "react-router-dom";
 
 import useStyles from "./styles";
+import useAlan from "./Alan";
 
 import { Actors, MovieInformation, Movies, NavBar, Profile } from "./";
 
 const App = () => {
   const classes = useStyles();
+  const alanBtnContainer = useRef();
+
+  useAlan();
 
   return (
     <div className={classes.root}>
@@ -24,6 +28,7 @@ const App = () => {
           <Route exact path="/profile/:id" element={<Profile />} />
         </Routes>
       </main>
+      <div ref={alanBtnContainer}></div>
     </div>
   );
 };
